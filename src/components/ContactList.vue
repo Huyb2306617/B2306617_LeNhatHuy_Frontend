0d5f90fc97b0d88eed7466cca8b18eda103f5196
Huyb2306617 <huyb2306617@student.ctu.edu.vn>
2026-07-07 17:25:34 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/ContactList.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  props: {
+    contacts: {
+      type: Array,
+      default: () => [],
+    },
+    activeIndex: {
+      type: Number,
+      default: -1,
+    },
+  }, // <-- Dấu ngoặc nhọn này đóng props lại ở đây!
+
+  emits: ["update:activeIndex"], // Đứng ngang hàng với props
+
+  methods: { // Đứng ngang hàng với props
+    updateActiveIndex(index) {
+      this.$emit("update:activeIndex", index);
+    },
+  },
+};
+</script>
+
+<template>
+  <ul class="list-group">
+    <li
+      class="list-group-item"
+      :class="{ active: index === activeIndex }"
+      v-for="(contact, index) in contacts"
+      :key="contact._id" 
+      @click="updateActiveIndex(index)"
+    >
+      {{ contact.name }}
+    </li>
+  </ul>
+</template>
